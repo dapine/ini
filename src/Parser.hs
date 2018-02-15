e81e0e78ea0fc25e2282877d3fa9a27f944a3280
David <davepinh@gmail.com>
2018-02-15 14:28:38 -0300

    Parsers added (parseKey, parseSectionName, parseInteger, parseDouble, parseBoolean)

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,10 +1,50 @@
-module Parser where
+module Parser
+    ( parseKey
+    , parseSectionName
+    , parseInteger
+    , parseDouble
+    , parseBoolean
+    ) where
 
 import Text.ParserCombinators.Parsec
+import Control.Monad
+import Numeric
 
 data INIParser = INIKey String
                | INIString String
                | INIInteger Integer
                | INIDouble Double
                | INIBool Bool 
-               | INISection (String, [(String, INIParser)]) deriving (Eq, Show)
+               | INISectionName String
+               | INISection (INIParser, [(String, INIParser)]) deriving (Eq, Show)
+
+parseKey :: Parser INIParser
+parseKey = do
+    k <- many1 alphaNum
+    return $ INIKey k
+
+parseSectionName :: Parser INIParser
+parseSectionName = do
+    char '['
+    sn <- many1 alphaNum
+    char ']'
+    return $ INISectionName sn
+
+-- FIXME: not supporting negative numbers
+parseInteger :: Parser INIParser
+parseInteger = liftM (INIInteger . read) $ many1 digit
+
+-- FIXME: not supporting negative numbers
+parseDouble :: Parser INIParser
+parseDouble = do
+    dec <- many1 digit
+    char '.'
+    fl <- many1 digit
+    return $ INIDouble $ fst . head . readFloat $ dec ++ "." ++ fl
+
+parseBoolean :: Parser INIParser
+parseBoolean = do
+    x <- (string "true" <|> string "false")
+    return $ case x of
+               "true" -> INIBool True
+               "false" -> INIBool False
